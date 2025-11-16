dd9795c5b1f7a3385f10f09894ba6567df07087a
tanvir <tanviralam451@gmail.com>
2025-11-16 23:57:10 +0600

    lab-4-EF

---
+++ lab-4-EF/lab-4-EF/Controllers/StudentController.cs
@@ -0,0 +1,31 @@
+﻿using lab_4_EF.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab_4_EF.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        UMSEntities db = new UMSEntities();
+        public ActionResult Index()
+        {
+            var student1 = new Student();
+            student1.Name = "Jalal";
+            student1.Address = "Dhaka, Bangladesh";
+            student1.Email = "std@gmail.com";
+
+            db.Students.Add(student1);
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult List()
+        {
+            var data = db.Students.ToList();
+            return View(data);
+        }
+    }
+}
---
+++ lab-4-EF/lab-4-EF/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="lab_4_EF.MvcApplication" Language="C#" %>
