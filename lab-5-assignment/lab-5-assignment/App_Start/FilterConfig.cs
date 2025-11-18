885e0512eda4b2171467f8396753696d282232de
tanvir <tanviralam451@gmail.com>
2025-11-18 19:29:01 +0600

    lab-5-assignment

---
+++ lab-5-assignment/lab-5-assignment/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace lab_5_assignment
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ lab-5-assignment/lab-5-assignment/Controllers/ProductController.cs
@@ -0,0 +1,79 @@
+﻿using lab_5_assignment.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab_5_assignment.Controllers
+{
+    public class ProductController : Controller
+    {
+        public readonly ProductDBEntities db = new ProductDBEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ViewBag.Categories = db.Categories.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();
+            return RedirectToAction("ProductList");
+        }
+
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            var product = db.Products.Find(id);
+            return View(product);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var product = db.Products.Find(id);
+            ViewBag.Categories = db.Categories.ToList();
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            db.Entry(product).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("ProductList");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var product = db.Products.Find(id);
+            return View(product);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("ProductList");
+        }
+
+        [HttpGet]
+        public ActionResult ProductList()
+        {
+            var products = db.Products.ToList();
+            return View(products);
+        }
+    }
+}
